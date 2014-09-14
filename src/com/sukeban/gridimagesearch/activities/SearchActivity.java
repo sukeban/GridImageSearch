c4f276daea40124b56145e0663603da9b9e272db
Cynthia Maxwell <cmaxwell@yahoo-inc.com>
2014-09-14 11:02:55 -0700

    upload gif

--- src/com/sukeban/gridimagesearch/activities/SearchActivity.java
+++ src/com/sukeban/gridimagesearch/activities/SearchActivity.java
@@ -20,7 +20,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+//import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
